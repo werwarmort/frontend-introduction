5e9adccf6861f68b42e80367696bad4020adb92f
Werwarmort <werwarmort256@gmail.com>
2025-04-22 13:24:36 +0300

    refactor lectures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
+import lectures from "@/widgets/LectureContent/config/lecturesConfig";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>
-      test
-    </div>
-  );
+    <main>
+      <h1>Лекции</h1>
+      <ul>
+        {lectures.map(({ id, title }) => (
+          <li key={id}>
+            <Link href={`/lectures/${id}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
---
+++ src/shared/lectures/index.ts
@@ -0,0 +1,19 @@
+import { lecture_1 } from './lecture_1'
+import { lecture_2 } from './lecture_2'
+import { lecture_3 } from './lecture_3'
+import { lecture_4 } from './lecture_4'
+import { lecture_5 } from './lecture_5'
+import { lecture_6 } from './lecture_6'
+import { lecture_7 } from './lecture_7'
+import { lecture_8 } from './8_lecture_architecture'
+
+export const lecturesHtml = {
+  '1': lecture_1,
+  '2': lecture_2,
+  '3': lecture_3,
+  '4': lecture_4,
+  '5': lecture_5,
+  '6': lecture_6,
+  '7': lecture_7,
+  '8': lecture_8,
+}
