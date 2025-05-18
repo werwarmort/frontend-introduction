4dc1c9dbb95f12e888a27605e5e3d68072b2e7ae
Werwarmort <werwarmort256@gmail.com>
2025-05-18 19:14:08 +0300

    добавил лабы на главную страницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link'
-import lectures from "@/widgets/LectureContent/config/lecturesConfig";
-import styles from './mainPage.module.scss'
+import Link from 'next/link';
+import lectures from '@/widgets/LectureContent/config/lecturesConfig';
+import labs from '@/widgets/LectureContent/config/labsConfig';
+import styles from './mainPage.module.scss';
 
 export default function HomePage() {
   return (
@@ -9,10 +10,25 @@ export default function HomePage() {
       <ul>
         {lectures.map(({ id, title }) => (
           <li key={id} className={styles.lectureLi}>
-            <span className={styles.index}>{id}</span><Link className={styles.lectureLink} href={`/lectures/${id}`}>{title}</Link>
+            <span className={styles.index}>{id}</span>
+            <Link className={styles.lectureLink} href={`/lectures/${id}`}>
+              {title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <h1 className={styles.mainTitle}>Лабораторные работы</h1>
+      <ul>
+        {labs.map(({ id, title }) => (
+          <li key={id} className={styles.lectureLi}>
+            <span className={styles.index}>{id}</span>
+            <Link className={styles.lectureLink} href={`/lab/${id}`}>
+              {title}
+            </Link>
           </li>
         ))}
       </ul>
     </main>
-  )
+  );
 }
